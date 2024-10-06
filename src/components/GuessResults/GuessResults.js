a57aa6aabb54dcb069837619a8d883a1f4c4740b
Irene Riportella <riportellairene@gmail.com>
2024-10-06 22:21:18 +0200

    feat: ✨ excercise-3 use Guess component

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { range } from "../../utils";
+import Guess from "../Guess/Guess";
 
 function GuessResults({ guesses }) {
+  // console.log(guesses);
+
   return (
     <div className="guess-results">
-      {guesses.map(({ value, id }, index) => (
-        <p
-          key={id}
-          className="guess"
-        >
-          {value}
-        </p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess
+          key={num}
+          value={guesses[num]}
+        />
       ))}
     </div>
   );
